7acfdd3b65b6ec342e41eafe64de380f6d9aa0c4
Sofiene29 <99467922+Sofiene29@users.noreply.github.com>
2022-10-21 16:23:51 +0200

    Update myTimer.h

--- MesDrivers/Include/myTimer.h
+++ MesDrivers/Include/myTimer.h
@@ -1,26 +1,29 @@
-#ifndef MYTIMER_H
-#define MYTIMER_H
+#ifndef MYGPIO_H
+#define MYGPIO_H
 #include "stm32f10x.h"
-typedef struct
-{
-	TIM_TypeDef * Timer ; // TIM1 à TIM4
-	unsigned short ARR ;
-	unsigned short PSC ;
-} MyTimer_Struct_TypeDef ;
-/*
-*****************************************************************************************
-* @brie f
-* @param -> Paramè t r e sous forme d ’ une s t r u c t u r e ( son a d re s se ) con ten an t l e s
-i n f o rm a ti o n s de base
-* @Note -> F onc ti on à l a n c e r s y s t ématiquement avan t d ’ a l l e r pl u s en dé t a i l dans l e s
-con f pl u s f i n e s (PWM, codeur i n c . . . )
-*************************************************************************************************
-*/
-void MyTimer_Base_Init ( TIM_TypeDef*Timer, unsigned short ARR, unsigned short PSC ) ;
-void MyTimer_Base_Start (TIM_TypeDef*Timer) ;
-void MyTimer_Base_Stop (TIM_TypeDef*Timer) ;
-void MyTimer_Active_IT ( TIM_TypeDef * Timer , char Prio,void(*Fonction_IT)(void) );
-void MyTimer_PWM( TIM_TypeDef * Timer , char Channel );
-void Set_Cycle(TIM_TypeDef * Timer, char Channel, char Percent);
+
+typedef struct {
 	
+	GPIO_TypeDef * GPIO ;
+	char GPIO_Pin ; //numero de 0 a 15
+	char GPIO_Conf ; // voir ci dessous
+	
+} MyGPIO_Struct_TypeDef ;
+
+#define In_Floating 0x4 // 0100
+#define In_PullDown 0x8 // 1000
+#define In_PullUp 0x8 //1000
+#define In_Analog 0x0 // 0000
+#define Out_Ppull 0x2 // 0010
+#define Out_OD 0x6// 0110
+#define AltOut_Ppull 0xA // 1010
+#define AltOut_OD 0xD // 1110
+
+void MyGPIO_Init_clck (void);
+void MyGPIO_Init ( GPIO_TypeDef * GPIO , int GPIO_Pin, char GPIO_Conf) ;
+int  MyGPIO_Read ( GPIO_TypeDef * GPIO , int GPIO_Pin ) ; // renvoie 0 ou autre chose different de 0
+void MyGPIO_Set ( GPIO_TypeDef * GPIO , int GPIO_Pin ) ;
+void MyGPIO_Reset ( GPIO_TypeDef * GPIO , int GPIO_Pin ) ;
+void MyGPIO_Toggle ( GPIO_TypeDef * GPIO , int GPIO_Pin ) ;
+
 #endif
