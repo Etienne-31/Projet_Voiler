18762408a770438db75c1e5326c0d3dea9962365
Etienne-31 <90270401+Etienne-31@users.noreply.github.com>
2022-11-25 17:58:09 +0100

    on récupère l'identité mais pas plus

--- activie2/Objects/mygpio.d
+++ activie2/Objects/mygpio.d
@@ -7,4 +7,3 @@
 .\objects\mygpio.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
 .\objects\mygpio.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\mygpio.o: C:\Programdata\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
-.\objects\mygpio.o: U:\4 IR\ProjetVoilier\Projet_Voilier\MesDrivers\Include\myGPIO.h
--- activie2/Source/Principal.c
+++ activie2/Source/Principal.c
@@ -2,25 +2,50 @@
 #include "myGPIO.h"
 #include "myTimer.h"
 #include "myADC.h"
+#include "MySPI.h"
 #define VALOUT 2
 
-int result ;
-
+char result ;
+/*
 void CallBack(){
 	//MyGPIO_Toggle(GPIOA,5);
 	result = ConvertChannel(ADC1,3) ;
-}
+}*/
 
 int main ( void )
 {
-
-	MyGPIO_Init(GPIOA,VALOUT,5);
+	
+	
+	//char result;
+	MySPI_Init(SPI1);  //Init de :la spi
+	MySPI_Clear_NSS();
+	MySPI_Send(0x00);
+	MySPI_Set_NSS();
+	MySPI_Clear_NSS();
+	result= MySPI_Read();
+	MySPI_Set_NSS();// pour recup id E5
+	//SPI1->POWER_CTL |= (1<<3);
+	//measure a 1 
+/*	MySPI_Send(0x2D);
+	MySPI_Send((1<<3));
+	MySPI_Set_NSS();
+	//X0
+	while(1){
+		MySPI_Clear_NSS();
+		MySPI_Send(0x32);
+		MySPI_Set_NSS();
+		MySPI_Clear_NSS();
+		result= MySPI_Read();
+		MySPI_Set_NSS();
+	}*/
+	
+/*	MyGPIO_Init(GPIOA,VALOUT,5);
 	MyTimer_Base_Init(TIM2,0x6000,0x6000);
 	MyTimer_Base_Start(TIM2);
 	RCC->CFGR |= (10<<14);
 	MyTimer_Active_IT ( TIM2 , 1,CallBack );
 	MyADC_Init(ADC1,3);
-	
+	*/
 	
 /*	MyGPIO_Init(GPIOA,0,7);
 	MyTimer_Base_Init(TIM2,0x6000,0x6000);
